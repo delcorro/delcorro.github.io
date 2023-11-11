09165accc629a8604a5b4e084866a9b24fc39da7
cris <delcorrocristianelias3@gmail.com>
2023-11-11 19:07:06 -0300

    formulariojs

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// definir valor del ticket
+const ticket = 200;
+
+// definir los descuentos
+
+let descEstudiante = 0.80;
+let descTrainee = 0.50;
+let descJunior = 0.15;
+
+// tomar elementos
+
+const formulario = document.getElementById('formulario');
+const categoria = document.getElementById('categoria');
+const cantidad = document.getElementById('cantidad');
+const totalPagar = document.getElementById('totalAPagar');
+const btnResumen = document.getElementById('resumen');
+const textoDescuento = document.getElementById('importeDescuento');
+
+function calcularPago(){
+    let total = cantidad.value * ticket;
+    // condicionar por descuento
+
+    switch (categoria.value) {
+        case "estudiante":
+            total = total - (total * descEstudiante);
+            textoDescuento.textContent = `Obtuvo un descuento de $ ${total * descEstudiante }`;
+            totalPagar
+            break;
+        
+        case "trainee":
+            total = total - (total * descTrainee);
+            textoDescuento.textContent = `Obtuvo un descuento de $ ${total * descEstudiante }`;
+            break;  
+        case "junior":
+            total = total - (total * descJunior)
+            textoDescuento.textContent = `Obtuvo un descuento de $ ${total * descEstudiante }`;
+            break
+        
+        default:
+            textoDescuento.textContent = `No tiene descuento`;
+            break;
+    }
+
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
+
+}
+
+btnResumen.addEventListener('click',(e)=>{
+    e.preventDefault()
+    calcularPago()
+});
